21440571994fc1669ed34ade94f57fba1286f6f4
aiden <117980553+aidenESL@users.noreply.github.com>
2023-03-09 20:07:02 +0100

    push

---
+++ README.md
@@ -0,0 +1,32 @@
+
+# Astro portfolio with TypeScript and Tailwind CSS
+
+
+
+## Screenshots
+
+![App Screenshot](https://zupimages.net/up/23/10/vyou.png)
+
+
+
+## Usage/Examples
+
+
+Run the following command on your local environment:
+```javascript
+git clone --depth=1 https://github.com/aidenEpitech/aiden-portfolio.git
+cd my-project-name
+npm install
+```
+Then, you can run locally in development mode with live reload:
+```javascript
+npm run dev
+```
+Open http://localhost:3000 with your favorite browser to see your project.
+
+## Authors
+
+- [@aidenEpitech](https://github.com/aidenEpitech)
+
+
+
---
+++ src/data/info.ts
@@ -0,0 +1,78 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Aiden MEBARKI",
+  jobDescription: "Web Developer",
+  about: `
+  Passionné de développement web depuis l'âge de 13 ans,
+  j'ai commencé par créer des serveurs Minecraft en apprenant le JAVA. En autodidacte, j'ai perfectionné mes compétences dans plusieurs langages
+  et travaille actuellement en freelance en attendant de commencer mon alternance en développement web en septembre 2023..
+  `,
+
+  experience: [
+    {
+      name: "Epitech",
+      location: "Lyon, France",
+      startDate: "Nov 2022",
+      endDate: "Présent",
+      description: [
+        "✓ Apprentissage des langages basique (HTML, CSS) ",
+        "✓ Approfondissement des langages WEB ainsi que de certains framwork (JavaScript, NodeJS, AstroJS, Laravel) ",
+        "✓ Ainsi que d'autres langages pour mon plaisir personnel (Python, C#, C++)",
+        "✓ Travail de groupe",
+        "✓ Rendre un projet avec une date limite, en respectant un cahier des charges",
+      ],
+    },
+    {
+      name: "42",
+      location: "Lyon, France",
+      startDate: "2021",
+      endDate: "2021",
+      description: [
+        "✓ Apprentissage poussé du C.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Cuisinier",
+      location: "Villefranche sur saone",
+      startDate: "2022",
+      endDate: "2022",
+      description: [
+        "✓ Respect des consignes strictes",
+        "✓ Adaptation de chaque commande en fonction d’un résident",
+        "✓ Avoir à respecter un planning dans les délais",
+      ],
+    },
+    {
+      name: "Responsable Adjoint de Cuisine",
+      location: "Villefranche sur saone",
+      startDate: "2020",
+      endDate: "2022",
+      description: [
+        "✓ S’adapter à la demande de la clientèle",
+        "✓ Travail d’équipe",
+        "✓ Gestion d’un budget",
+        "✓ Gérer une équipe d’une dizaine de personne",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    twitter: "https://twitter.com/aidenESL",
+    github: "https://github.com/aidenEpitech",
+    email: "sofiane-aiden.mebarki@epitech.eu",
+    linkedin: "https://www.linkedin.com/in/aidenepitech/",
+  },
+
+  projects: [
+    {
+      title: "API Discord for Deezer",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "#",
+      liveUrl: "#",
+    },
+  ],
+};
