7f0026510bd252c569262499ba9204027465c691
aiden ツ <aiden@aiden.local>
2023-05-09 13:44:50 +0200

    v 2.0

--- src/data/info.ts
+++ src/data/info.ts
@@ -3,9 +3,9 @@ export const info = {
   name: "Aiden MEBARKI",
   jobDescription: "Web Developer",
   about: `
-  Passionné de développement web depuis l'âge de 13 ans,
-  j'ai commencé par créer des serveurs Minecraft en apprenant le JAVA. En autodidacte, j'ai perfectionné mes compétences dans plusieurs langages
-  et travaille actuellement en freelance en attendant de commencer mon alternance en développement web en septembre 2023..
+  Avec une passion dévorante pour le développement web dès l'âge de 13 ans,
+  j'ai débuté mon parcours en créant des serveurs Minecraft avec le langage Java. Autodidacte dans l'âme, j'ai affiné mes compétences en explorant différents langages informatiques.
+  Actuellement étudiant à Epitech, je suis à la recherche d'une alternance en développement web pour septembre 2023, afin de mettre à profit mon savoir-faire.
   `,
 
   experience: [
@@ -15,11 +15,12 @@ export const info = {
       startDate: "Nov 2022",
       endDate: "Présent",
       description: [
-        "✓ Apprentissage des langages basique (HTML, CSS) ",
-        "✓ Approfondissement des langages WEB ainsi que de certains framwork (JavaScript, NodeJS, AstroJS, Laravel) ",
-        "✓ Ainsi que d'autres langages pour mon plaisir personnel (Python, C#, C++)",
-        "✓ Travail de groupe",
-        "✓ Rendre un projet avec une date limite, en respectant un cahier des charges",
+        "✓ Développement web : HTML, CSS, JavaScript, PHP, Symfony, React, Node, Angular, Laravel... ",
+        "✓ Bases de données : MySQL, MongoDB. ",
+        "✓ Conception de sites web : UX, accessibilité.",
+        "✓ Architecture logicielle : modèle MVC, bibliothèques et frameworks.",
+        "✓ Sécurité informatique : vulnérabilités, cryptographie.",
+		"✓ Développement en équipe : Git, Jenkins, environnement agile.",
       ],
     },
     {
@@ -28,7 +29,13 @@ export const info = {
       startDate: "2021",
       endDate: "2021",
       description: [
-        "✓ Apprentissage poussé du C.",
+        "✓ La philosophie et la méthodologie de l'école 42.",
+		"✓ Les fondamentaux de la programmation en langage C.",
+		"✓ Les algorithmes et les structures de données.",
+		"✓ La manipulation de l'environnement Unix/Linux.",
+		"✓ La pratique du pair programming.",
+		"✓ La participation à des projets en équipe.",
+		
       ],
     },
   ],
@@ -36,7 +43,7 @@ export const info = {
   education: [
     {
       name: "Cuisinier",
-      location: "Villefranche sur saone",
+      location: "Les Magnolias - Villefranche sur saone",
       startDate: "2022",
       endDate: "2022",
       description: [
@@ -47,7 +54,7 @@ export const info = {
     },
     {
       name: "Responsable Adjoint de Cuisine",
-      location: "Villefranche sur saone",
+      location: "Léon - Villefranche sur saone",
       startDate: "2020",
       endDate: "2022",
       description: [
@@ -60,19 +67,28 @@ export const info = {
   ],
 
   socialMedia: {
-    twitter: "https://twitter.com/aidenESL",
     github: "https://github.com/aidenEpitech",
-    email: "sofiane-aiden.mebarki@epitech.eu",
+    email: "mailto:sofiane-aiden.mebarki@epitech.eu",
     linkedin: "https://www.linkedin.com/in/aidenepitech/",
   },
 
   projects: [
+	{
+		title: "Karmine Corp IOS APP",
+		isFeatured: true,
+		thumbnail: "/assets/images/karminecorp-app.png",
+		githubUrl: "https://github.com/aidenEpitech/Karmine-Corp-App",
+		// liveUrl: "#",
+	  },
     {
       title: "API Discord for Deezer",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "#",
-      liveUrl: "#",
+      thumbnail: "/assets/images/API-Discord-for-Deezer.png",
     },
+	{
+		title: "Minecraft Anti-Cheat for EpiCube",
+		isFeatured: true,
+		thumbnail: "/assets/images/epicube-anticheat.png",
+	  },
   ],
 };
